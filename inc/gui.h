6947bc71f844a29344de5815a0a323085b72d54b
Basil DALIÉ <dalie.basil@gmail.com>
2016-10-07 11:58:13 +0200

    Ajout gui.h

---
+++ inc/gui.h
@@ -0,0 +1,18 @@
+#ifndef __GUI_H__
+#define __GUI_H__
+
+#include "../inc/field.h"
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+typedef struct rgb_color {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+} rgb_color;
+
+void generate_colors(rgb_color colors[], int number);
+bool init(SDL_Window** window, SDL_Renderer** renderer);
+void update(SDL_Renderer* renderer, struct grid* field, rgb_color colors[]);
+
+#endif // __GUI_H__
