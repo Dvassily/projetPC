5c9ee404bae334f2d6cf639767c8d4f1b2fc53ab
Basil Dalié <dalie.basil@gmail.com>
2016-12-04 21:35:25 +0100

    Ajout synchro -t2 (etape 2)

--- src/n-threads-simulation-synchro-sem.c
+++ src/n-threads-simulation-synchro-sem.c
@@ -2,64 +2,55 @@
 #include <fcntl.h>
 #include "../inc/n-threads-simulation-no-synchro.h"
 
+#define AREA_LOCK 4;
+
 sem_t field_lock[DEFAULT_GRID_WIDTH][DEFAULT_GRID_HEIGHT];
-sem_t lock_lock;
 
-void move_prologue(person p, direction d) {
-    // New position of the field->people[p]
-    int x = p.origin_x;
-    int y = p.origin_y;
-
-    if (x <= 0) return;
-    
-    if (d == N) {
-	for (int i = x; i < x + 4; ++i)
-	    sem_wait(&field_lock[i][y - 1]);
-    } else if (d == NW) {
-	for (int i = x + 2; i >= x; --i)
-	    sem_wait(&field_lock[i][y - 1]);
-	for (int i = y - 1; i < (y - 1) + 4; ++i)
-	    sem_wait(&field_lock[x - 1][i]);
-    } else if (d == W) {
-	for (int i = y; i < y + 4; ++i)
-	    sem_wait(&field_lock[x - 1][i]);
-    } else if (d == SW) {
-	for (int i = x + 2; i >= x; --i)
-	    sem_wait(&field_lock[i][y + 4]);
-	for (int i = y + 4; i > y; --i)
-	    sem_wait(&field_lock[x - 1][i]);
-    } else if (d == S) {
-	for (int i = x; i < x + 4; ++i)
-	    sem_wait(&field_lock[i][y + 4]);
+void move_prologue(int x, int y) {
+    for (int i = x + 3;  i >= x; --i) {
+	sem_wait(&field_lock[i][y - 1]);
+	sem_wait(&field_lock[i][y + 4]);
     }
+
+    for (int i = y - 1; i <= y + 4; ++i) {
+	sem_wait(&field_lock[x - 1][i]);	
+    }
+	
+    /*
+    for (int i = x + 3;  i >= x - 1; --i) {
+	for (int j = y - 1; j < y + 5; ++j) {
+	    //SDL_Delay(6);
+	    sem_wait(&field_lock[i][j]);
+	}
+	}*/
 }
 
-void move_epilogue(person p, direction d) {
-    // New position of the field->people[p]
-    int x = p.origin_x;
-    int y = p.origin_y;
+void move_epilogue(int x, int y) {
+    for (int i = x + 3;  i >= x; --i) {
+	sem_post(&field_lock[i][y - 1]);
+	sem_post(&field_lock[i][y + 4]);
+    }
 
-    if (d == N) {
-	for (int i = x; i < x + 4; ++i)
-	    sem_post(&field_lock[i][y + 4]);
-    } else if (d == NW) {
-	for (int i = y + 1; i < y + 1 + 4; ++i)
-	    sem_post(&field_lock[x + 4][i]);
-	for (int i = x + 1; i < x + 1 + 3; ++i)
-	    sem_post(&field_lock[i][y + 4]);
-    } else if (d == W) {
-	for (int i = y; i < y + 4; ++i)
-	    sem_post(&field_lock[x + 4][i]);
-	
-    } else if (d == SW) {
-	for (int i = y - 1; i < y - 1 + 4; ++i)
-	    sem_post(&field_lock[x + 4][i]);
-	for (int i = x + 1; i < x + 1 + 3; ++i)
-	    sem_post(&field_lock[i][y - 1]);
-    } else if (d == S) {
-	for (int i = x; i < x + 4; ++i)
-	    sem_post(&field_lock[i][y - 1]);
-    } 
+    
+    for (int i = y - 1; i <= y + 4; ++i) {
+	sem_post(&field_lock[x - 1][i]);	
+    }
+
+    
+    /*
+    for (int i = x - 1; i < x + 4; ++i) {
+	for (int j = y + 4; j >= y - 1; --j) {
+	    //SDL_Delay(6);
+	    sem_post(&field_lock[i][j]);	    
+	}
+	}*/
+    /*
+    for (int i = x + 3; i >= x - 1; --i) {
+	for (int j = y - 1; j < y + 5; ++j) {
+	    SDL_Delay(6);
+	    sem_post(&field_lock[i][j]);
+	}
+	}*/
 }
 
 void unlock_exit(person p) {
@@ -73,45 +64,48 @@ void unlock_exit(person p) {
 void* n_threads_simulation_synchro_sem(void* ptr_args)
 {
     grid* field = ((thread_args*) ptr_args)->field;
-    unsigned person_id = ((thread_args*) ptr_args)->person_id;
+    unsigned p = ((thread_args*) ptr_args)->person_id;
 
     int i = 0;
-    while (! is_finished(field) && field->people[person_id].status == IN) {
+    while (! is_finished(field) && field->people[p].status == IN) {
 	++i;
 
-	for (unsigned p = 0; p < field->person_count; ++p) {
-	    if (field->people[p].status == IN && p == person_id) {
-		double min_dist = distance_to_azimuth(field->people[p].origin_x,
-						      field->people[p].origin_y);
-		direction dir = UNKNOWN_DIR;
-		
-		for (int m = 0; m < NB_MOVES; ++m) {
-		    if (can_move(field, field->people[p], (::direction) m)) {
-			int x = field->people[p].origin_x + moves[m].x;
-			int y = field->people[p].origin_y + moves[m].y;
+	if (field->people[p].status == IN && p == p) {
+	    double min_dist = distance_to_azimuth(field->people[p].origin_x,
+						  field->people[p].origin_y);
+	    direction dir = UNKNOWN_DIR;
 
-			float dist = distance_to_azimuth(x, y);
+	    int old_x = field->people[p].origin_x;
+	    int old_y = field->people[p].origin_y;
+
+	    move_prologue(old_x, old_y);
+	    for (int m = 0; m < NB_MOVES; ++m) {
+		if (can_move(field, field->people[p], (::direction) m)) {
+		    int x = field->people[p].origin_x + moves[m].x;
+		    int y = field->people[p].origin_y + moves[m].y;
+
+		    float dist = distance_to_azimuth(x, y);
 			
-			if (min_dist == 0 || dist < min_dist) {
-			    min_dist = dist;
-			    dir = (::direction) m;
-			}
+		    if (min_dist == 0 || dist < min_dist) {
+			min_dist = dist;
+			dir = (::direction) m;
 		    }
 		}
-
-		if (dir == UNKNOWN_DIR)
-		    continue;
-
-		//move_prologue(field->people[p], dir);
-		move_person(field, p, dir);
-		//move_epilogue(field->people[p], dir);
-		
-		if (field->people[p].origin_x == 0) {
-		    field->people[p].status = OUT;
-		    delete_person(field, p);
-		    unlock_exit(field->people[p]);
-		}
 	    }
+
+	    if (dir == UNKNOWN_DIR) {
+		move_epilogue(old_x, old_y);
+		continue;
+	    }
+
+	    move_person(field, p, dir);
+		
+	    if (field->people[p].origin_x == 0) {
+		field->people[p].status = OUT;
+		delete_person(field, p);
+	    }
+
+	    move_epilogue(old_x, old_y);
 	}
 
 	#ifdef GUI
@@ -132,7 +126,6 @@ void* n_threads_simulation_synchro_sem(void* ptr_args)
     
     thread_args t_args[field->person_count];
     pthread_t thread[field->person_count];
-    sem_init(&lock_lock, 0, 1);    
 
     for (unsigned i = 0; i < DEFAULT_GRID_WIDTH; ++i)
 	for (unsigned j = 0; j < DEFAULT_GRID_HEIGHT; ++j)
